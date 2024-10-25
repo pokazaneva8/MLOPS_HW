4b4cc50a8ce9139437920ed9b8d264c47d2298c0
polina <pokazaneva8@yandex.ru>
2024-10-26 00:23:14 +0500

    "12345"

--- HW_Pokazaneva/test.py
+++ HW_Pokazaneva/test.py
@@ -2,6 +2,7 @@ import random
 
 random_sequence = [random.randint(1, 1000) for _ in range(100)]
 
+
 def generate_fibonacci_sequence(limit):
     fibonacci_numbers = [0, 1]
     while True:
@@ -9,11 +10,10 @@ def generate_fibonacci_sequence(limit):
         if next_fib > limit:
             break
         fibonacci_numbers.append(next_fib)
-    return set(fibonacci_numbers)  
+    return set(fibonacci_numbers)
+
 
 max_value = max(random_sequence)
 fibonacci_numbers = generate_fibonacci_sequence(max_value)
-
 fibonacci_in_sequence = [num for num in random_sequence if num in fibonacci_numbers]
-
-print("Числа Фибоначчи в рандомной последовательности:", fibonacci_in_sequence)
+print("Числа Фибоначчи в случайной последовательности:", fibonacci_in_sequence)
